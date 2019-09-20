52d193f809bad2d2b02e104cf2f2bba85d1e52ce
IFFANCOE <vbaako1@hotmail.com>
2019-09-20 23:52:14 +0700

    test5

--- src/ch.js
+++ src/ch.js
@@ -4,13 +4,13 @@ import './ch.css';
 
 class ch extends React.Component {
     render() {
-        let count_end = this.props.check_count > 5 ? 'area2' : '';
-        let count_ends = this.props.check_count > 5 ? "Game Over" : "Counter : " + this.props.check_count;
-        let test = `area ${count_end}`
+        let count_1 = this.props.check_count > 5 ? 'area2' : '';
+        let count_2 = this.props.check_count > 5 ? "Game Over" : "Counter : " + this.props.check_count;
+        let test = `area ${count_1}`
         return (
             <div>
                 <div className={test}>
-                <h1 className={test}>{this.count_end}{count_ends}</h1>
+                <h1 className={test}>{this.count_1}{count_2}</h1>
                 </div>
             </div>
         )
